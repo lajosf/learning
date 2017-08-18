f918bf09bae0688f7bb38f6f65bb1eb08ebd4471
lajosf <lajosf@outlook.com>
2017-08-18 11:07:15 +0200

    Adding unit test learning

---
+++ Microsoft/LINQ/Linq/Linq/ExtensionMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Linq
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string myStringDouble = "108,1";
+            double myDouble = myStringDouble.ToDouble();
+            Console.WriteLine("Heres is my double :) = " + myDouble);
+            Console.ReadLine();
+        }
+    }
+
+    public static class StringExtension {
+        static public double ToDouble(this string data){
+            double result = double.Parse(data);
+            return result;
+        } 
+    }
+}
---
+++ Microsoft/UnitTest/BankTest/BankTest/BankAccountTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankAccountNS;
+
+namespace BankTest
+{
+    [TestFixture()]
+    public class BankAccountTest
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
